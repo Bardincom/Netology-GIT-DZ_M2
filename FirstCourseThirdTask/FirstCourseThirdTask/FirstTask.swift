43fefeb41afd761f4126c45f1ccc549207464abd
Bardincom <master_als@icloud.com>
2019-12-13 23:48:13 +0200

    M2 Third Home work: first code received
    Первая часть кодового слова: TURoJ

---
+++ FirstCourseThirdTask/FirstCourseThirdTask/FirstTask.swift
@@ -0,0 +1,84 @@
+//
+//  FirstTask.swift
+//  FirstCourseThirdTask
+//
+//  Created by Aleksey Bardin on 13.12.2019.
+//  Copyright © 2019 E-Legion. All rights reserved.
+//
+
+import Foundation
+import FirstCourseThirdTaskChecker
+
+/// Вам необходимо создать два сабкласса ArrayInitializableStorage. Один из них должен
+/// реализовать стек(LIFO), а второй - очередь(FIFO). Список методов и их требования
+/// смотрите в комментариях к классу ArrayInitializableStorage и AbstractStorage.
+/// Для проверки передайте экземпляры этих классов в метод. Если ваше решение окажется
+/// правильным, то в консоли вы увидите часть кодового слова.
+
+class StackArrayInializableStorage: ArrayInitializableStorage {
+  override var count: Int {
+    return array.count
+  }
+
+  var array: [Int]
+  
+  override func push(_ element: Int) {
+    array.append(element)
+    print(array)
+  }
+  
+  override func pop() -> Int {
+    print(array)
+    return array.removeLast()
+  }
+  
+/// инициализация хранилища
+  required init(array: [Int]) {
+    self.array = array
+    super.init(array: array)
+  }
+  
+/// инициализация пустого массива
+  required init() {
+    self.array = []
+    super.init()
+  }
+  
+
+  
+}
+
+let stack = StackArrayInializableStorage()
+
+
+class Queue: ArrayInitializableStorage {
+  override var count: Int {
+    return array.count
+  }
+  var  array: [Int]
+  
+  override func push(_ element: Int) {
+    array.append(element)
+    print(array)
+  }
+  
+  override func pop() -> Int {
+    print(array)
+    return array.removeFirst()
+  }
+  
+  
+  required init(array: [Int]) {
+    self.array = array
+    super.init(array: array)
+  }
+  
+  required init() {
+    self.array = []
+    super.init()
+  }
+  
+}
+
+let queue = Queue()
+
--- FirstCourseThirdTask/FirstCourseThirdTask/main.swift
+++ FirstCourseThirdTask/FirstCourseThirdTask/main.swift
@@ -1,1 +1,7 @@
 // Этот файл пуст не по ошибке. В этот раз вам необходимо самостоятельно импортировать необходимые модули и запустить проверку
+import Foundation
+import FirstCourseThirdTaskChecker
+
+let checker = Checker()
+
+checker.checkInheritance(stack: stack, queue: queue)
